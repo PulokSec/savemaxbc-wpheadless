b774226f546328e881167056833fd2671090eddd
Pulok Chowdhury <riopulok@gmail.com>
2023-12-07 01:24:17 +0600

    added new page

---
+++ src/app/selling-services/page.tsx
@@ -0,0 +1,213 @@
+import { gql } from '@apollo/client';
+import { Metadata } from 'next';
+
+import { getClient } from '@/lib/apollo';
+
+import SharedBanner from '@/components/elements/SharedBanner';
+import AdviceServiceFeature from '@/components/service-menu-components/AdviceServiceFeature';
+import BottomServiceSection from '@/components/service-menu-components/BottomServiceSection';
+import Footer from '@/components/shared/Footer';
+
+const query = gql`
+  query {
+    pages(where: { id: 66968 }) {
+      nodes {
+        seo {
+          title
+          description
+          canonicalUrl
+          focusKeywords
+          openGraph {
+            image {
+              url
+            }
+          }
+          jsonLd {
+            raw
+          }
+        }
+        sellingServices {
+          bannerSection {
+            bannerImage {
+              sourceUrl
+            }
+            bannerHeading
+            bannerDescription
+          }
+          topFeatureDescription
+          topFeatureTitle
+          serviceFeatureSection {
+            featureTitle
+            featureDescription
+            featuredDiv {
+              title
+              description
+              image {
+                sourceUrl
+                altText
+              }
+            }
+          }
+          bottomSection {
+            backgroundImage {
+              sourceUrl
+              altText
+            }
+            description
+          }
+        }
+      }
+    }
+    settingsOptions {
+      savemaxOptions {
+        headerSettings {
+          uploadLogo {
+            sourceUrl
+            altText
+          }
+        }
+        generalSettings {
+          schemaProductRating
+        }
+        footerSettings {
+          socialUrl {
+            facebook
+            twitter
+            instagram
+          }
+          copyrightText
+          footerLeftWidget {
+            title
+            phone
+            emailAddress
+            address
+          }
+          footerLogoSection {
+            logoText
+            logoUpload {
+              altText
+              sourceUrl
+            }
+          }
+        }
+      }
+    }
+    menus(where: { location: MENU_2 }) {
+      nodes {
+        name
+        slug
+        menuItems(first: 50) {
+          nodes {
+            url
+            target
+            parentId
+            label
+            cssClasses
+            description
+            id
+            childItems {
+              nodes {
+                uri
+                label
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+export async function generateMetadata(): Promise<Metadata> {
+  const { data } = await getClient().query({
+    query,
+    context: {
+      fetchOptions: {
+        next: { revalidate: 5 },
+      },
+    },
+  });
+  return {
+    title: data?.pages?.nodes[0]?.seo?.title,
+    description: data?.pages?.nodes[0]?.seo?.description,
+    robots: { index: false, follow: false },
+
+    // icons: {
+    //   icon: '/favicon/favicon.ico',
+    //   shortcut: '/favicon/favicon-16x16.png',
+    //   apple: '/favicon/apple-touch-icon.png',
+    // },
+    manifest: `/favicon/site.webmanifest`,
+    openGraph: {
+      url: 'https://savemaxbc.com/',
+      title: data?.pages?.nodes[0]?.seo?.title,
+      description: data?.pages?.nodes[0]?.seo?.description,
+      siteName: 'https://savemaxbc.com/',
+      images: data?.pages?.nodes[0]?.seo?.openGraph?.image?.url,
+      type: 'website',
+      locale: 'en_US',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: data?.pages?.nodes[0]?.seo?.title,
+      description: data?.pages?.nodes[0]?.seo?.description,
+      // images: [`${siteConfig.url}/images/og.jpg`],
+      creator: '@PulokSec',
+    },
+    authors: [
+      {
+        name: 'Cansoft Tech',
+        url: 'https://cansoft.com/',
+      },
+    ],
+  };
+}
+
+export default async function SellingServices() {
+  const { data } = await getClient().query({
+    query,
+    context: {
+      fetchOptions: {
+        next: { revalidate: 5 },
+      },
+    },
+  });
+  console.log(data);
+  return (
+    <>
+      <main className='max-w-screen mt-10 bg-[url("https://savemaxheadlessdemo.csoft.ca/wp-content/uploads/2023/10/Middle-part-bg.png")] bg-cover bg-center bg-no-repeat'>
+        <SharedBanner
+          bannerData={data?.pages?.nodes[0]?.sellingServices?.bannerSection}
+          headerData={data?.menus?.nodes[0]?.menuItems?.nodes}
+          settingsData={data?.settingsOptions?.savemaxOptions?.headerSettings}
+        />
+        <div className='mt-20 px-5 md:mt-40 lg:mt-40'>
+          <div className='text-center'>
+            <h1 className='w-full text-center text-2xl md:text-3xl lg:text-5xl'>
+              {data?.pages?.nodes[0]?.sellingServices?.topFeatureTitle}
+            </h1>
+            <div
+              className='md:text-md mt-5 px-5 text-center text-xs leading-6 lg:px-20 lg:text-lg'
+              dangerouslySetInnerHTML={{
+                __html:
+                  data?.pages?.nodes[0]?.sellingServices?.topFeatureDescription,
+              }}
+            ></div>
+          </div>
+        </div>
+        <AdviceServiceFeature
+          featuredData={
+            data?.pages?.nodes[0]?.sellingServices?.serviceFeatureSection
+              ?.featuredDiv
+          }
+        />
+        <BottomServiceSection
+          bottomSection={data?.pages?.nodes[0]?.sellingServices?.bottomSection}
+        />
+        <Footer
+          navigation={data?.menus?.nodes[0]?.menuItems?.nodes}
+          settingsData={data?.settingsOptions?.savemaxOptions?.footerSettings}
+        />
+      </main>
+    </>
+  );
+}
