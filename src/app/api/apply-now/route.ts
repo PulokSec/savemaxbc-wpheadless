456a2d43fd9755802bf1ff2c280a480b54fdb11a
Pulok <riopulok@gmail.com>
2023-12-19 00:43:11 +0600

    bug fix

--- src/app/api/apply-now/route.ts
+++ src/app/api/apply-now/route.ts
@@ -2,6 +2,9 @@ import { render } from '@react-email/render';
 import { NextRequest, NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
 
+import { sendEmail } from '@/lib/email-helper';
+
+import AutoReply from '@/components/utils/email-template/auto_reply';
 import ApplyNowEmail from '@/components/utils/email-template/contact_form';
 
 const smtpOptions = {
@@ -14,19 +17,12 @@ const smtpOptions = {
   },
 };
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
 const transporter = nodemailer.createTransport({
   ...smtpOptions,
 });
 export async function POST(request: NextRequest) {
   const formData: FormData = await request.formData();
-  const body = Object.fromEntries(formData);
-  // console.log(body);
-  console.log(body);
+
   try {
     const chunks: Buffer[] = [];
     const cv = formData.get('cv') as File;
@@ -51,17 +47,17 @@ export async function POST(request: NextRequest) {
       ),
       attachments: attachments,
     });
-    // await sendEmail({
-    //   from: formData.get('fromEmail') as string,
-    //   to: formData.get('mail') as string,
-    //   subject: formData.get('emailSubject') as string,
-    //   cc: formData.get('cc') as string,
-    //   html: render(
-    //     AutoReply({
-    //       name: formData.get('name') as string,
-    //     })
-    //   ),
-    // });
+    await sendEmail({
+      from: formData.get('fromEmail') as string,
+      to: formData.get('mail') as string,
+      subject: formData.get('emailSubject') as string,
+      cc: formData.get('cc') as string,
+      html: render(
+        AutoReply({
+          name: formData.get('name') as string,
+        })
+      ),
+    });
     return NextResponse.json(
       { message: 'Form Submission Success' },
       { status: 201 }
