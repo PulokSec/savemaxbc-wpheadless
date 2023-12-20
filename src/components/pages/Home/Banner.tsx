354c01d6e26c1864ea4edc854f7f722c0bf12bb5
Pulok <riopulok@gmail.com>
2023-12-20 22:30:59 +0600

    fix home banner

--- src/components/pages/Home/Banner.tsx
+++ src/components/pages/Home/Banner.tsx
@@ -72,10 +72,10 @@ export default function Banner(props: MyProps) {
       <Header navigation={headerData} settingsData={settingsData} />
       <div className='mx-auto py-16'>
         <div className='mx-auto mt-[15%] flex w-full flex-col items-center justify-center px-5 text-center md:mt-[10%] md:px-10'>
-          <p className='text-leading-3 text-lg font-black text-white md:text-5xl'>
+          <p className='text-leading-3 text-lg font-black text-white md:text-5xl lg:text-7xl'>
             {bannerData?.bannerHeading}
           </p>
-          <p className='text-leading-3 text-md mt-5 font-semibold text-white md:text-3xl'>
+          <p className='text-leading-3 text-md mt-5 font-semibold text-white md:text-3xl lg:text-5xl'>
             {bannerData?.bannerSubtitle}
           </p>
           <div className='mt-10 flex items-center justify-center gap-x-6'>
