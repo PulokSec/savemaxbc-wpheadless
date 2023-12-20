d794e8034170a0a8c67452ec2e836ee427f47aa4
Arif <iariful950@gmail.com>
2023-12-20 23:32:36 +0600

    about banner

--- src/components/elements/AboutBanner.tsx
+++ src/components/elements/AboutBanner.tsx
@@ -26,7 +26,7 @@ export default function AboutBanner(props: MyProps) {
   return (
     <>
       <div
-        className='relative h-[50vh] w-full bg-[length:100%_50vh] bg-center bg-no-repeat md:hidden md:bg-[length:100%_100vh] lg:h-[100vh]'
+        className='relative h-[70vh] w-full bg-[length:100%_70vh] bg-center bg-no-repeat md:hidden md:bg-[length:100%_100vh] lg:h-[100vh]'
         style={{
           backgroundImage: `url(${bannerData?.bannerImage?.sourceUrl})`,
         }}
@@ -51,13 +51,13 @@ export default function AboutBanner(props: MyProps) {
         </div>
       </div>
       <div
-        className='relative hidden w-full bg-[length:100%_60vh] bg-center bg-no-repeat md:block md:bg-[length:100%_100vh] lg:h-[100vh]'
+        className='relative hidden h-[60vh] w-full bg-center bg-no-repeat md:block md:bg-[length:100%_60vh] xl:h-[100vh] xl:bg-[length:100%_100vh]'
         style={{
           backgroundImage: `url(${bannerData?.bannerImagePc?.sourceUrl})`,
         }}
       >
         <Header navigation={headerData} settingsData={settingsData} />
-        <div className='mt-24 flex flex-col items-center justify-center lg:mt-60'>
+        <div className='mt-40 flex flex-col items-center justify-center lg:mt-60'>
           <div className='py-0 lg:py-10'>
             <div className='flex w-full flex-col items-center justify-center px-5 lg:px-10'>
               <p className='w-full text-center text-3xl font-bold uppercase leading-5 text-white md:text-3xl lg:text-7xl'>
